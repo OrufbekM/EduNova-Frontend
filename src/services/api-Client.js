42a115f87b7a4a8a4e76e983ede34b1ed2433c65
OrufbekM <orufbekmaxmudjonov77@gmail.com>
2026-02-01 20:29:06 +0500

    update apiClient

--- src/services/api-Client.js
+++ src/services/api-Client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken, deleteToken } from './token';
 
-const baseURL = import.meta.env.API_BASE_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.API_BASE_URL || "https://eduboard-dste.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL
